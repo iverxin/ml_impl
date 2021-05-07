b47d05f0bb233ff0b807a0d363d48e43fa29b34a
spade <spadeaiverxin@163.com>
2021-05-07 19:55:33 +0800

    SVM smo完善。增加启发式参数选择。

--- svm/svm.py
+++ svm/svm.py
@@ -16,20 +16,22 @@ class SVM:
         self.b = 0
         self.w = 0
         # hyparamters
-        self.MAX_ITER_NUM = 10
+        self.MAX_ITER_NUM = 100
         # 惩罚参数，是个超参数。越大对误分类的惩罚越大。
-        self.C = 100 #TODO the C need to init the some method
+        self.C = 10  # TODO the C need to init the some method
+        self.tol = 1e-3  # 容忍度，越小说明对KKT条件越苛刻
+        self.E = np.zeros(self.example_num)
 
-    def rand_j(self, i, m ):
+    def rand_j(self, i, m):
         """
         generate a index j of alpha2 different with i
         :param i: the alpha i
         :param m:
         :return: j index
         """
-        j = np.random.randint(0,m)
-        while(j==i):
-            j = np.random.randint(0,m)
+        j = np.random.randint(0, m)
+        while (j == i):
+            j = np.random.randint(0, m)
         return j
 
     def fxi(self, x_):
@@ -50,7 +52,7 @@ class SVM:
         ans = 0
         for i in range(self.example_num):
             ans += self.alpha[i] * self.Y[i] * self.kernel(self.X[i], x_)
-        ans+= self.b
+        ans += self.b
         return ans
 
     def predict(self, x_):
@@ -60,16 +62,19 @@ class SVM:
 
     def kernel(self, X1, X2, kernel_type='default', **kwargs):
         # TODO 加入其他的kernel方法
-        if kernel_type=="default" or kernel_type=="linear":
-            return np.dot(X1, X2.T) # linear SVM
-        if kernel_type=="polynomial":
-            return np.dot(X1, X2.T)**(kwargs['d'])
+        if kernel_type == "default" or kernel_type == "linear":
+            return np.dot(X1, X2.T)  # linear SVM
+        if kernel_type == "polynomial":
+            return np.dot(X1, X2.T) ** (kwargs['d'])
 
-    def smo(self):
-
+    def smo_b(self):
+        """
+        smo算法，alpha1和alpha2为随机选择。
+        :return:
+        """
         iter_num = 0
-        while(iter_num<self.MAX_ITER_NUM):
-            iter_num+=1
+        while (iter_num < self.MAX_ITER_NUM):
+            iter_num += 1
 
             for i in range(self.example_num):
 
@@ -88,32 +93,95 @@ class SVM:
                 # calculate the boundary L and H
                 y1 = self.Y[index1]
                 y2 = self.Y[index2]
-                if(y1!=y2):
-                    L =max(0, alpha2-alpha1)
-                    H = min(self.C, self.C+ alpha2-alpha1)
+                if (y1 != y2):
+                    L = max(0, alpha2 - alpha1)
+                    H = min(self.C, self.C + alpha2 - alpha1)
                 else:
-                    L = max(0, alpha2+alpha1-self.C)
-                    H = min(self.C, alpha2 +alpha1)
+                    L = max(0, alpha2 + alpha1 - self.C)
+                    H = min(self.C, alpha2 + alpha1)
 
                 # calculate the eta
-                eta = self.kernel(self.X[index1], self.X[index1])+\
-                    self.kernel(self.X[index2], self.X[index2]) - \
-                    2.*self.kernel(self.X[index1], self.X[index2])
+                K11 = self.kernel(self.X[index1], self.X[index1])
+                K21 = self.kernel(self.X[index2], self.X[index1])
+                K22 = self.kernel(self.X[index2], self.X[index2])
+                K12 = self.kernel(self.X[index1], self.X[index2])
+                eta = K11 + K22 - 2. * K12
 
                 # update the alpha2
-                if eta==0:
-                    eta +=1e-6
-                alpha2_unclip = alpha2+y2*(E1-E2)/eta
+                if eta == 0:
+                    eta += 1e-6
+                alpha2_unclip = alpha2 + y2 * (E1 - E2) / eta
 
                 # clip alpha2
-                alpha2_new = np.clip(alpha2_unclip,L,H)
+                alpha2_new = np.clip(alpha2_unclip, L, H)
                 # calculate alpha1_new
-                alpha1_new = alpha1+y1*y2*(alpha2-alpha2_new)
+                alpha1_new = alpha1 + y1 * y2 * (alpha2 - alpha2_new)
 
                 # update to weights
                 self.alpha[index1] = alpha1_new
                 self.alpha[index2] = alpha2_new
 
+                # update b
+                b1 = -E1 - y1 * K11 * (alpha1_new - alpha1) - y2 * K21 * (alpha2_new - alpha2) + self.b
+                b2 = -E2 - y2 * K12 * (alpha1_new - alpha1) - y2 * K22 * (alpha2_new - alpha2) + self.b
+                self.b = (b1 + b2) / 2
+
+    def smo_a(self):
+        """
+        基于启发式算法确定alpha1和alpha2
+        :return:
+        """
+        iter_num = 0
+        while iter_num < self.MAX_ITER_NUM:
+            iter_num += 1
+            for index1 in range(0, self.example_num):
+                alpha1 = self.alpha[index1]
+                self.E[index1] = self.fxi(self.X[index1]) - self.Y[index1]
+                y1 = self.Y[index1]
+
+                if (self.E[index1] * y1 > self.tol and alpha1 > self.tol) or (self.E[index1] * y1 < 0 and alpha1 < self.C):
+                    # alpha1违反KKT条件。
+                    # 开始寻找alpha2，选择E1-E2差值最大的。
+                    delta = self.E[index1] - self.E
+                    index2 = np.argmax(delta)
+                    alpha2 = self.alpha[index2]
+                    self.E[index2] = self.fxi(self.X[index2]) - self.Y[index2]
+
+                    # calculate the boundary L and H
+                    y1 = self.Y[index1]
+                    y2 = self.Y[index2]
+                    if (y1 != y2):
+                        L = max(0, alpha2 - alpha1)
+                        H = min(self.C, self.C + alpha2 - alpha1)
+                    else:
+                        L = max(0, alpha2 + alpha1 - self.C)
+                        H = min(self.C, alpha2 + alpha1)
+
+                    # calculate the eta
+                    K11 = self.kernel(self.X[index1], self.X[index1])
+                    K21 = self.kernel(self.X[index2], self.X[index1])
+                    K22 = self.kernel(self.X[index2], self.X[index2])
+                    K12 = self.kernel(self.X[index1], self.X[index2])
+                    eta = K11 + K22 - 2. * K12
+
+                    # update the alpha2
+                    if eta == 0:
+                        eta += 1e-6
+                    alpha2_unclip = alpha2 + y2 * (self.E[index1] - self.E[index2]) / eta
+
+                    # clip alpha2
+                    alpha2_new = np.clip(alpha2_unclip, L, H)
+                    # calculate alpha1_new
+                    alpha1_new = alpha1 + y1 * y2 * (alpha2 - alpha2_new)
+
+                    # update to weights
+                    self.alpha[index1] = alpha1_new
+                    self.alpha[index2] = alpha2_new
+
+                    # update b
+                    b1 = -self.E[index1] - y1 * K11 * (alpha1_new - alpha1) - y2 * K21 * (alpha2_new - alpha2) + self.b
+                    b2 = -self.E[index2] - y2 * K12 * (alpha1_new - alpha1) - y2 * K22 * (alpha2_new - alpha2) + self.b
+                    self.b = (b1 + b2) / 2
 
 
 def loadDataSet(dataspath):
@@ -138,19 +206,16 @@ def main():
         if Y[i] == 0:
             Y[i] = -1
     svm_model = SVM(X, Y)
-    svm_model.smo()
+    svm_model.smo_a()
 
-    for i in range(45,60):
-        y_pred = svm_model.predict(X[i])
-        print("predict:{} \n label:{}".format(y_pred, Y[i]))
+
+    y_pred = svm_model.predict(X)
+    rights = (y_pred == Y).astype('float')
+    rights = rights.sum() / rights.shape[0]
+    print("准确率：{}%".format(rights*100))
     # test the kernel function
-    x = np.array([1,2,3])
-    y = np.array([4,5,6])
-    print(svm_model.kernel(x,y))
-    #
-    # print(svm_model.X.shape)
-    # print(svm_model.Y.shape)
+
 
 if __name__ == '__main__':
     main()
-    print("hello")
+    print("Finished")
